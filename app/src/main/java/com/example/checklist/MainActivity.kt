97494c5abb9212730705b2b2853b785c31480a6a
ACER <fakhrikhairi03@gmail.com>
2019-10-21 19:40:52 +0700

    second commit

---
+++ app/src/main/java/com/example/checklist/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.checklist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import android.R.layout.simple_list_item_1
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val language = listOf("Kotlin","Python","Ruby","JavaScript","PHP")
+
+        list_view.adapter = ArrayAdapter(this, simple_list_item_1, language)
+
+        list_view.setOnItemClickListener{ parent, view, position, id ->
+            Toast.makeText(this,"Anda memilih: ${language[position]}",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+}
